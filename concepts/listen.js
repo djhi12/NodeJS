fde76485e12c6c2aa3c31f23177350ca1ca0a555
djhi12 <ido20002@byui.edu>
2023-10-18 16:38:01 +0800

    Initial commit

---
+++ concepts/listen.js
@@ -0,0 +1,14 @@
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, Node.js!');
+});
+
+const port = 3000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+    console.log(`Server is listening on http://${host}:${port}`);
+});
